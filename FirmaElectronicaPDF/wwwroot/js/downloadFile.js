3b6bcfb0d5434109692a3a3110968d9724e2e750
Hennry Chan <hennry.chan@blueoceantech.com.mx>
2024-09-06 08:20:43 -0600

    Agregar archivos de proyecto.

---
+++ FirmaElectronicaPDF/wwwroot/js/downloadFile.js
@@ -0,0 +1,15 @@
+﻿/**
+ * Realiza la descarga del archivo PDF 
+ * 
+ * @param {any} pdfBytes PDF en formarto byte
+ * @param {any} nameFile Nombre del documento PDF
+ */
+function downloadPDF(pdfBytes, nameFile) {
+    const byteArray = new Uint8Array(pdfBytes);
+    const blob = new Blob([byteArray], { type: 'application/pdf' });
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = nameFile;
+    link.click();
+    URL.revokeObjectURL(link.href);
+}
